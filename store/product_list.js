591722c7eb297f2e3cff7ec58e6fe7f8bd7aaf92
neerajdhurandher-deloitte <101085508+neerajdhurandher-deloitte@users.noreply.github.com>
2023-08-22 01:03:55 +0530

    create store section with product card and modification in cart.

---
+++ store/product_list.js
@@ -0,0 +1,137 @@
+export let product_list = [
+    {
+        "id": "0001",
+        "title": "Believer",
+        "artist": "Imagine dragons",
+        "album": "Believer",
+        "year": "2018",
+        "category": "DVD",
+        "in_stock": "available"
+    },
+    {
+        "id": "0002",
+        "title": "Believer",
+        "artist": "Imagine dragons",
+        "album": "Believer",
+        "year": "2018",
+        "category": "DVD",
+        "in_stock": "available"
+    },
+    {
+        "id": "0003",
+        "title": "Believer",
+        "artist": "Imagine dragons",
+        "album": "Believer",
+        "year": "2018",
+        "category": "DVD",
+        "in_stock": "available"
+    },
+    {
+        "id": "0004",
+        "title": "Believer",
+        "artist": "Imagine dragons",
+        "album": "Believer",
+        "year": "2018",
+        "category": "DVD",
+        "in_stock": "available"
+    },
+    {
+        "id": "0005",
+        "title": "Believer",
+        "artist": "Imagine dragons",
+        "album": "Believer",
+        "year": "2018",
+        "category": "DVD",
+        "in_stock": "available"
+    },
+    {
+        "id": "0001",
+        "title": "Believer",
+        "artist": "Imagine dragons",
+        "album": "Believer",
+        "year": "2018",
+        "category": "DVD",
+        "in_stock": "available"
+    },
+    {
+        "id": "0002",
+        "title": "Believer",
+        "artist": "Imagine dragons",
+        "album": "Believer",
+        "year": "2018",
+        "category": "DVD",
+        "in_stock": "available"
+    },
+    {
+        "id": "0003",
+        "title": "Believer",
+        "artist": "Imagine dragons",
+        "album": "Believer",
+        "year": "2018",
+        "category": "DVD",
+        "in_stock": "available"
+    },
+    {
+        "id": "0004",
+        "title": "Believer",
+        "artist": "Imagine dragons",
+        "album": "Believer",
+        "year": "2018",
+        "category": "DVD",
+        "in_stock": "available"
+    },
+    {
+        "id": "0005",
+        "title": "Believer",
+        "artist": "Imagine dragons",
+        "album": "Believer",
+        "year": "2018",
+        "category": "DVD",
+        "in_stock": "available"
+    },
+    {
+        "id": "0001",
+        "title": "Believer",
+        "artist": "Imagine dragons",
+        "album": "Believer",
+        "year": "2018",
+        "category": "DVD",
+        "in_stock": "available"
+    },
+    {
+        "id": "0002",
+        "title": "Believer",
+        "artist": "Imagine dragons",
+        "album": "Believer",
+        "year": "2018",
+        "category": "DVD",
+        "in_stock": "available"
+    },
+    {
+        "id": "0003",
+        "title": "Believer",
+        "artist": "Imagine dragons",
+        "album": "Believer",
+        "year": "2018",
+        "category": "DVD",
+        "in_stock": "available"
+    },
+    {
+        "id": "0004",
+        "title": "Believer",
+        "artist": "Imagine dragons",
+        "album": "Believer",
+        "year": "2018",
+        "category": "DVD",
+        "in_stock": "available"
+    },
+    {
+        "id": "0005",
+        "title": "Believer",
+        "artist": "Imagine dragons",
+        "album": "Believer",
+        "year": "2018",
+        "category": "DVD",
+        "in_stock": "available"
+    }
+]
